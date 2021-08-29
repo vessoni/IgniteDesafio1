5c08f9c5096df5e681176ebc4676594c648decaf
Alexandre Vessoni <av.vessoni@gmail.com>
2021-08-29 22:32:18 +0100

    Added

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const newTask = {
+      id: tasks.length + 1, title: newTaskTitle, isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task);
+    
+    console.log(editTask);
+    setTasks(editTask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+    setTasks(removeTask);
   }
 
   return (
